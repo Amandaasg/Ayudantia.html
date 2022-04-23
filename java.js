1d9fda7fe90fd029d3fbc00e5a1b0857bfafac57
Amandaasg <amanda.allendes@uc.cl>
2022-04-23 13:21:09 -0400

    tarea de java

---
+++ java.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    var boton = $("#ocultarboton");
+    boton.click(function(){
+        $("#Foto1").fadeTo("fast",0)
+    });
+
+    var boton2 = $("#aparecerboton");
+    boton2.click(function(){
+        $("#Foto1").fadeTo("fast",1)
+    });
+ 
+    var texto = $("#Texto");
+    texto.click(function(){
+        $(this).text("Mensaje oculto")
+    });
+
+    var foto2 = $("#Foto2");
+    foto2.mouseenter(function(){
+        $(this).css("background-color", "pink");
+    });
+
+});
